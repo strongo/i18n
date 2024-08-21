2cd718088b6e5b0d19434667f27717541c3e6302
Alexander Trakhimenok <533159+trakhimenok@users.noreply.github.com>
2024-08-21 08:10:48 +0100

    feat: TranslationContext interface changed to extend LocalesProvider interface

--- i18n_context.go
+++ i18n_context.go
@@ -4,32 +4,28 @@ import "context"
 
 // TranslationContext is an i18n context (internationalization)
 type TranslationContext interface {
+	LocalesProvider
 	GetTranslator(c context.Context) Translator
-	SupportedLocales() LocalesProvider
 	SetLocale(code5 string) error
 }
 
 func NewContext(c context.Context, supportedLocales LocalesProvider) TranslationContext {
-	return &translationContext{ctx: c, supportedLocales: supportedLocales}
+	return &translationContext{ctx: c, LocalesProvider: supportedLocales}
 }
 
 type translationContext struct {
 	ctx                context.Context
-	supportedLocales   LocalesProvider
 	locale             Locale
 	translatorProvider TranslatorProvider
+	LocalesProvider
 }
 
 func (l10n *translationContext) GetTranslator(_ context.Context) Translator {
 	return l10n.translatorProvider(l10n.locale.Code5)
 }
 
-func (l10n *translationContext) SupportedLocales() LocalesProvider {
-	return l10n.supportedLocales
-}
-
 func (l10n *translationContext) SetLocale(code5 string) error {
-	locale, err := l10n.supportedLocales.GetLocaleByCode5(code5)
+	locale, err := l10n.LocalesProvider.GetLocaleByCode5(code5)
 	if err != nil {
 		errorf(l10n.ctx, "*WebhookContextBase.SetLocate(%v) - %v", code5, err)
 		return err
